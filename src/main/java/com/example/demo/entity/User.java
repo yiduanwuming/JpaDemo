df1113890971bc05b6b64febdd661c451f7ffb6b
yiduanwuming <1098046904>
2021-02-03 16:41:16 +0800

    更换电脑及数据库信息

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -11,7 +11,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private String id;
+    private int id;
 
     @Column(name = "name")
     private String name;
